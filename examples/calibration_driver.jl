3f2877a2838ddae89fa28a91b45f78781daa4cee
tonyewong <aewsma@rit.edu>
2024-06-17 09:15:58 -0400

    intiial update to use mcmcdiagnostictools psrf

--- examples/calibration_driver.jl
+++ examples/calibration_driver.jl
@@ -15,9 +15,20 @@ mkpath(output_dir)
 
 calibration_start_year = 1850
 calibration_end_year   = 2017
-total_chain_length     = 20_000_000
-size_subsample         = 10_000
+#total_chain_length     = 20_000_000
+#size_subsample         = 10_000
+total_chain_length     = 200000
+size_subsample         = 10
 threshold_gr           = 1.1
+
+##TESTING##
+model_config="brick"; burnin_length=100; num_walkers=2; start_from_priors=false; calibration_data_dir = nothing;
+#
+x = MimiBRICK.run_calibration(output_dir = output_dir, model_config="brick", calibration_start_year=calibration_start_year,
+                    calibration_end_year=calibration_end_year, total_chain_length=total_chain_length,
+                    burnin_length=burnin_length, threshold_gr=threshold_gr, num_walkers=2,
+                    size_subsample=size_subsample, start_from_priors=false)
+##END TESTING##
 
 # BRICK calibration
 x = MimiBRICK.run_calibration(output_dir = output_dir, model_config="brick", calibration_start_year=calibration_start_year,
