c7bbcb644f667f31b653b51fcb08b060a7444343
Tony Wong <aewsma@rit.edu>
2026-05-06 16:11:41 -0400

    Update test_downscaling.jl
    removing checking gelman and rubin diagnostic from testing since that will be really finnicky with such a short chain

--- test/test_downscaling.jl
+++ test/test_downscaling.jl
@@ -22,7 +22,7 @@ for model_config in ["brick", "sneasybrick", "doeclimbrick"]
     
     # run calibration
     MimiBRICK.run_calibration(output_dir=tmp_dir, model_config=model_config, calibration_start_year=1850, calibration_end_year=2017,
-                        total_chain_length=total_chain_length, burnin_length=0, threshold_gr=threshold_gr, num_walkers=2,
+                        total_chain_length=total_chain_length, burnin_length=0, threshold_gr=threshold_gr, check_gr=false, num_walkers=2,
                         size_subsample=size_subsample, start_from_priors=false)
 
     # run hindcast and projections 
